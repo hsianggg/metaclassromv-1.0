839023513c2cd30738c956b8a4a533f5f818402d
shin7777731 <134361192+shin7777731@users.noreply.github.com>
2023-07-22 16:03:31 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,133 @@
 <template>
-  <router-view/>
+  <h1>登入</h1>
+  <form @submit="handleSubmit">
+    <label>帳號:</label>
+    <input type="text" required v-model="account">
+    <label>密碼:</label>
+    <input type="password" required v-model="password">
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required>
+      <label>同意所有條款</label>
+    </div>
+    <div class="submit">
+      <button type="submit">確認</button>
+    </div>
+  </form>
+  <p>帳號: {{ account }}</p>
+  <p>密碼: {{ password }}</p>
+  <p>Terms accept: {{ terms }}</p>
+   <div>
+    <h1>錯誤</h1>
+    <p v-if="error" class="error">{{ error }}</p>
+  </div>
 </template>
+<script>
+// 在 Vue 組件中使用 axios
+import axios from 'axios';
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+export default {
+  data() {
+    return {
+      account: '',
+      password: '',
+      terms: false,
+      passwordError: ''
+    };
+  },
+  methods: {
+    handleSubmit(event) {
+      this.passwordError = this.password.length > 5 ? '' : '密碼至少需要6個字元';
+
+      if (!this.passwordError) {
+        const formData = {
+          account: this.account,
+          password: this.password,
+        };
+
+        axios.post('http://localhost:3000/api/login', formData)
+          .then(response => {
+            console.log(response.data);
+            this.account = response.data.account; // 將帳號值指派給 account 變數
+            this.password = response.data.password; // 將密碼值指派給 password 變數
+          })
+          .catch(error => {
+            console.error(error);
+            this.error = error.response.data; // 將伺服器回應的錯誤訊息儲存到 error 屬性中
+            // 處理錯誤回應
+            // 使用 window.alert() 顯示錯誤訊息
+        window.alert(this.error);
+          });
+      } else {
+        event.preventDefault(); // 防止表單提交
+      }
+    },
+  },
+  mounted() {
+    // 向伺服器發送 GET 請求
+    axios.get('http://localhost:3000/api/users')
+      .then(response => {
+        this.users = response.data; // 將伺服器返回的使用者資料存儲在 users 屬性中
+      })
+      .catch(error => {
+        console.error('Error fetching users from server:', error);
+      });
+  }
+};
+</script>
+<style>
+h1 {
+  color: black;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+form {
+  max-width: 540px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 15px 0 15px;
+  font-size: 1.2em;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 5px 10px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
-import './utils/libs/pollyfill'
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import './assets/main.css'
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
-
-
+createApp(App).mount('#app')
