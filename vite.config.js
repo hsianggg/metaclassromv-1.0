537d1881194962bdaefdc20dfddf68606988fb43
frank-rex <118089583+frank-rex@users.noreply.github.com>
2023-08-05 15:25:52 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,21 @@
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue()
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      path: "path-browserify",
+      '@': '/src',
+      'path': 'path-browserify'
     }
   },
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
   server: {
-    host: '0,0,0,0',
-    port : 8000,
+    host: '',
+    port: 3000,
   }, 
   define: {
     'process.env': {}
-  },
-  proxyTable: {
-    '/api': {
-      target: 'http://localhost:3000/',
-      ChangeOrigin: true
-    }
-  },
-})
+  }
+});
